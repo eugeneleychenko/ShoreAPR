37e4b18ac49617f7542a295b6dbed31c757fd9de
eugeneleychenko <nycspicebo@gmail.com>
2024-04-04 16:56:23 -0400

    Revert "4-4"
    This reverts commit 83f55858de96dff46182b4c023b42085c104f33f.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./styles.css";
 import APRForm from "./Calc";
+import logo from "./ShoreFunding.png";
 
 export default function App() {
   return (
     <div className="App">
-      <img
-        src="https://www.lendzi.com/wp-content/uploads/2020/10/logo.svg"
-        alt="Logo"
-        style={{ width: "70%" }}
-      />
+      <img src={logo} alt="Logo" style={{ width: "100%" }} />
       <APRForm />
     </div>
   );
